a2bdbc8cc7509fca4d316234df0a644379edf446
Johni DOuglas Marangon <johni.douglas.marangon@gmail.com>
2014-03-12 01:23:09 -0300

    new feature

--- data-structure/display_pointers/Principal.pas
+++ data-structure/display_pointers/Principal.pas
@@ -8,6 +8,13 @@ uses
   Vcl.ExtCtrls, Vcl.ComCtrls;
 
 type
+  PApontador = ^TNode;
+  TNode = record
+    Valor: String;
+    Proximo: PApontador;
+  end;
+
+
   TForm2 = class(TForm)
     PageControl1: TPageControl;
     TabSheet1: TTabSheet;
@@ -24,10 +31,23 @@ type
     Panel3: TPanel;
     BitBtn5: TBitBtn;
     StringGrid1: TStringGrid;
+    TabSheet4: TTabSheet;
+    Panel4: TPanel;
+    BitBtn6: TBitBtn;
+    GridPonterios: TStringGrid;
+    BitBtn7: TBitBtn;
+    BitBtn8: TBitBtn;
     procedure FormCreate(Sender: TObject);
     procedure BitBtn1Click(Sender: TObject);
     procedure BitBtn3Click(Sender: TObject);
     procedure BitBtn4Click(Sender: TObject);
+    procedure BitBtn6Click(Sender: TObject);
+    procedure BitBtn7Click(Sender: TObject);
+    procedure BitBtn8Click(Sender: TObject);
+
+
+  private
+    PrimeiroNo: PApontador;
   public
     VetorEstatico: array [0..4] of Integer;
     VetorDinamicoInt: array of Integer;
@@ -165,6 +185,78 @@ begin
   {
   pt:= PTypeInfo( @p );
   if ( pT^.Kind = tkWString ) then
+  }
+end;
+
+
+procedure TForm2.BitBtn6Click(Sender: TObject);
+var
+  NoAuxiliar: PApontador;
+begin
+  New( NoAuxiliar );  // alocando espaço na memória
+
+  NoAuxiliar.Proximo:=PrimeiroNo;
+  NoAuxiliar.Valor:= ( InputBox('', 'Digite uma string', '') );
+
+  PrimeiroNo:= NoAuxiliar;
+end;
+
+
+
+
+procedure TForm2.BitBtn7Click(Sender: TObject);
+var
+  NoAuxiliar: PApontador;
+  IdxGrid: Integer;
+  Tamanho: Integer;
+begin
+  IdxGrid:= 0;
+
+  NoAuxiliar:= PrimeiroNo;
+  while ( NoAuxiliar <> nil ) do
+  begin
+    Inc( IdxGrid );
+
+    GridPonterios.RowCount:= Succ( IdxGrid );
+    GridPonterios.Cells[0, ( IdxGrid )]:= 'ListaEncadeada(TNode)';
+    GridPonterios.Cells[1, ( IdxGrid )]:= IdxGrid.ToString;
+    GridPonterios.Cells[2, ( IdxGrid )]:= NoAuxiliar.Valor;
+    GridPonterios.Cells[3, ( IdxGrid )]:= Format( '%p', [NoAuxiliar] ) ;
+    GridPonterios.Cells[4, ( IdxGrid )]:= IntToStr( StrToInt('$' + Format( '%p', [NoAuxiliar] ) ) );
+
+    NoAuxiliar:= NoAuxiliar.Proximo;
+  end;
+
+end;
+
+procedure TForm2.BitBtn8Click(Sender: TObject);
+var
+  NoAuxiliar, NoAnterior: PApontador;
+  ValorASerExcluido: string;
+begin
+  ValorASerExcluido:= InputBox('', 'Digite uma string', '');
+
+  // caso tenha somente um registro
+  {
+  if ( PrimeiroNo.Proximo = nil ) or ( PrimeiroNo.Valor = ValorASerExcluido ) then
+  begin
+    NoAuxiliar:= PrimeiroNo;
+    PrimeiroNo:= PrimeiroNo.Proximo;
+    Dispose( NoAuxiliar );
+  end else
+  begin
+    NoAuxiliar:= PrimeiroNo;
+    while ( NoAuxiliar.Proximo <> nil ) do
+    begin
+      if ( NoAuxiliar.Proximo.Valor = ValorASerExcluido ) then
+      begin
+        NoAnterior:= NoAuxiliar; // pega o item anterio
+        NoAnterior.Proximo:= NoAuxiliar.Proximo.Proximo; // Mudar o ponteiro para excluir o item
+        Exit();
+      end;
+      NoAuxiliar:= NoAuxiliar.Proximo;
+    end;
+  end;
   }
 end;
 
